06ed0b69935dc2220f6e808a6accfa4dbcdb00c2
ra5ika27 <31758298+ra5ika27@users.noreply.github.com>
2018-06-25 21:02:40 -0700

    Changed URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,6 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //connecting to db
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGOLAB_COPPER_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
